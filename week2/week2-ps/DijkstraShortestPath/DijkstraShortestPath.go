08453c118a3a5432289ff34481892e62eec9301c
Benyamin Manullang <benjdewantara@gmail.com>
2021-04-02 18:45:35 +0700

    Embed MinHeap into the DijkstraShortestPath

--- week2/week2-ps/DijkstraShortestPath/DijkstraShortestPath.go
+++ week2/week2-ps/DijkstraShortestPath/DijkstraShortestPath.go
@@ -1,15 +1,25 @@
 package DijkstraShortestPath
 
 import "../Graph"
+import "../Heap"
 
 type DijkstraShortestPath struct {
-	Graph               Graph.Graph
-	VerticesCongregated []int
-	WeightsCongregated  []int
+	Graph                     Graph.Graph
+	VerticesCongregated       []int
+	WeightsCongregated        []int
+	MinHeapOfVerticesAdjacent Heap.MinHeap
 }
 
 func (d *DijkstraShortestPath) InitWithRoot(initialRootIndex int) {
 	edgeOfRoot := d.Graph.Edges[initialRootIndex]
 	d.VerticesCongregated = append(d.VerticesCongregated, edgeOfRoot.Tail)
 	d.WeightsCongregated = append(d.WeightsCongregated, 0)
+
+	d.MinHeapOfVerticesAdjacent = Heap.MinHeap{
+		Indices: edgeOfRoot.Heads[0:],
+		Values:  edgeOfRoot.Weights[0:],
+	}
+	d.MinHeapOfVerticesAdjacent.Heapify()
+}
+
 }
