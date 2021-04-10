ba45cf1c7ca488de6b339b6f42dfb48b1cf53146
Benyamin Manullang <benjdewantara@gmail.com>
2021-04-11 03:21:40 +0700

    Method PopulateHeapsFromFile

--- week3/week3-ps/main.go
+++ week3/week3-ps/main.go
@@ -1,7 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"./Heap"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
 
 func main() {
+	maxHeap := Heap.Heap{
+		IsMinHeap: false,
+		Indices:   make([]int, 0),
+		Values:    make([]int, 0),
+	}
+
+	minHeap := Heap.Heap{
+		IsMinHeap: true,
+		Indices:   make([]int, 0),
+		Values:    make([]int, 0),
+	}
+
+	PopulateHeapsFromFile(
+		"_6ec67df2804ff4b58ab21c12edcb21f8_Median.txt",
+		&minHeap,
+		&maxHeap)
+
 	fmt.Println("Hell on earth")
+}
+
+func PopulateHeapsFromFile(
+	filepath string,
+	minHeap *Heap.Heap,
+	maxHeap *Heap.Heap) {
+
+	contentBytes, _ := ioutil.ReadFile(filepath)
+	for intIndx, intStr := range strings.Split(string(contentBytes), "\n") {
+		intStr = strings.TrimSuffix(intStr, "\r")
+		num, _ := strconv.Atoi(intStr)
+
+		if len(minHeap.Values) == 0 {
+			minHeap.Insert(intIndx, num)
+			continue
+		} else if len(maxHeap.Values) == 0 {
+			maxHeap.Insert(intIndx, num)
+			continue
+		}
+
+		if num <= maxHeap.Values[0] {
+			maxHeap.Insert(intIndx, num)
+		} else {
+			minHeap.Insert(intIndx, num)
+		}
+
+		isMinHeapHoldingMore := len(minHeap.Values) >= len(maxHeap.Values)+2
+		isMaxHeapHoldingMore := len(maxHeap.Values) >= len(minHeap.Values)+2
+		if isMinHeapHoldingMore {
+			indx, value := minHeap.ExtractRoot()
+			maxHeap.Insert(indx, value)
+		} else if isMaxHeapHoldingMore {
+			indx, value := maxHeap.ExtractRoot()
+			minHeap.Insert(indx, value)
+		}
+	}
 }
