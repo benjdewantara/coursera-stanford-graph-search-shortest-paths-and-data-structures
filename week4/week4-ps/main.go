305e756e11c3fb3592cf4a6f179e50e09056d39e
Benyamin Manullang <benjdewantara@gmail.com>
2021-04-17 19:41:33 +0700

    Use map

--- week4/week4-ps/main.go
+++ week4/week4-ps/main.go
@@ -8,20 +8,21 @@ import (
 )
 
 func main() {
-	numbers := TargetValuesFromFilepath("./_6ec67df2804ff4b58ab21c12edcb21f8_algo1-programming_prob-2sum.txt")
+	numbers := NumbersFromFilepath("./_6ec67df2804ff4b58ab21c12edcb21f8_algo1-programming_prob-2sum.txt")
+
 	if numbers != nil {
 		fmt.Println("Populated")
 	}
 }
 
-func TargetValuesFromFilepath(
-	filepath string) []int {
-	numbers := make([]int, 0)
+func NumbersFromFilepath(
+	filepath string) map[int]uint8 {
+	numbers := make(map[int]uint8)
 
 	contentBytes, _ := ioutil.ReadFile(filepath)
 	for _, intStr := range strings.Split(string(contentBytes), "\n") {
 		num, _ := strconv.Atoi(intStr)
-		numbers = append(numbers, num)
+		numbers[num] = 1
 	}
 
 	return numbers
