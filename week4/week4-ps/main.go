bfca46721395d410f7a2cb4a6b7778cedd464bb0
Benyamin Manullang <benjdewantara@gmail.com>
2021-04-17 19:25:42 +0700

    Separated by \n instead of \r\n

--- week4/week4-ps/main.go
+++ week4/week4-ps/main.go
@@ -1,23 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	ReadFilepath("./_6ec67df2804ff4b58ab21c12edcb21f8_algo1-programming_prob-2sum.txt")
+	TargetValuesFromFilepath("./_6ec67df2804ff4b58ab21c12edcb21f8_algo1-programming_prob-2sum.txt")
 }
 
-func ReadFilepath(
+func TargetValuesFromFilepath(
 	filepath string) int {
 
 	sumMedians := 0
 
 	contentBytes, _ := ioutil.ReadFile(filepath)
-	for intIndx, intStr := range strings.Split(string(contentBytes), "\r\n") {
-		_ = intIndx
-		_ = intStr
+	for _, intStr := range strings.Split(string(contentBytes), "\n") {
+		num, _ := strconv.Atoi(intStr)
+		fmt.Println(num)
 	}
 
 	return sumMedians
