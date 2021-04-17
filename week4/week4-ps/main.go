67a3897f75c2d683a11565a91672df9226451ea4
Benyamin Manullang <benjdewantara@gmail.com>
2021-04-17 19:35:09 +0700

    These are just the numbers that will be summed to form a target value later. Not the target values themselves

--- week4/week4-ps/main.go
+++ week4/week4-ps/main.go
@@ -8,21 +8,21 @@ import (
 )
 
 func main() {
-	targetValues := TargetValuesFromFilepath("./_6ec67df2804ff4b58ab21c12edcb21f8_algo1-programming_prob-2sum.txt")
-	if targetValues != nil {
+	numbers := TargetValuesFromFilepath("./_6ec67df2804ff4b58ab21c12edcb21f8_algo1-programming_prob-2sum.txt")
+	if numbers != nil {
 		fmt.Println("Populated")
 	}
 }
 
 func TargetValuesFromFilepath(
 	filepath string) []int {
-	targetValues := make([]int, 0)
+	numbers := make([]int, 0)
 
 	contentBytes, _ := ioutil.ReadFile(filepath)
 	for _, intStr := range strings.Split(string(contentBytes), "\n") {
 		num, _ := strconv.Atoi(intStr)
-		targetValues = append(targetValues, num)
+		numbers = append(numbers, num)
 	}
 
-	return targetValues
+	return numbers
 }
