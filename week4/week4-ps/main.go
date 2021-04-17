b836d1fd2ef8e37f53a58f184aa1a44af9139285
Benyamin Manullang <benjdewantara@gmail.com>
2021-04-18 02:49:50 +0700

    Examining target value = -9967

--- week4/week4-ps/main.go
+++ week4/week4-ps/main.go
@@ -8,14 +8,49 @@ import (
 )
 
 func main() {
-	numbers := NumbersFromFilepath("./_6ec67df2804ff4b58ab21c12edcb21f8_algo1-programming_prob-2sum.txt")
+	numbers := NumberMapFromFilepath("./_6ec67df2804ff4b58ab21c12edcb21f8_algo1-programming_prob-2sum.txt")
+	usedNumbers := make(map[int]uint8)
+
+	countOfTargetValuesFormedByDistinctXAndY := 0
+
+	//for targetValue := -10000; targetValue <= 10000; targetValue++ {
+	for targetValue := -9967; targetValue <= -9967; targetValue++ {
+		for num := range numbers {
+			x := num
+			y := targetValue - x
+
+			_, xExists := numbers[x]
+			_, yExists := numbers[y]
+
+			bothExist := xExists && yExists
+			bothDifferent := x != y
+
+			if !bothExist || !bothDifferent {
+				continue
+			}
+
+			_, xUsed := usedNumbers[x]
+			_, yUsed := usedNumbers[y]
+
+			bothUnused := !xUsed && !yUsed
+
+			if bothExist && bothDifferent && bothUnused {
+				usedNumbers[x] = 1
+				usedNumbers[y] = 1
+			}
+		}
+	}
+
+	fmt.Println(
+		fmt.Sprintf(
+			"countOfTargetValuesFormedByDistinctXAndY = %d", countOfTargetValuesFormedByDistinctXAndY))
 
 	if numbers != nil {
 		fmt.Println("Populated")
 	}
 }
 
-func NumbersFromFilepath(
+func NumberMapFromFilepath(
 	filepath string) map[int]uint8 {
 	numbers := make(map[int]uint8)
 
