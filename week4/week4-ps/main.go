1c8c7adc250922cf122443143c0b40a389f24efe
Benyamin Manullang <benjdewantara@gmail.com>
2021-04-18 05:44:38 +0700

    Still failed attempt, this amounts to zero. I don't even know anymore

--- week4/week4-ps/main.go
+++ week4/week4-ps/main.go
@@ -9,12 +9,13 @@ import (
 
 func main() {
 	numbers := NumberMapFromFilepath("./_6ec67df2804ff4b58ab21c12edcb21f8_algo1-programming_prob-2sum.txt")
-	usedNumbers := make(map[int]uint8)
+	usedNumberExistenceCount := make(map[int]int)
 
 	countOfTargetValuesFormedByDistinctXAndY := 0
 
-	//for targetValue := -10000; targetValue <= 10000; targetValue++ {
-	for targetValue := -9967; targetValue <= -9967; targetValue++ {
+	for targetValue := -10000; targetValue <= 10000; targetValue++ {
+	//for targetValue := -9967; targetValue <= -9967; targetValue++ {
+		isEverythingDistinct := false
 		for num := range numbers {
 			x := num
 			y := targetValue - x
@@ -29,36 +30,55 @@ func main() {
 				continue
 			}
 
-			_, xUsed := usedNumbers[x]
-			_, yUsed := usedNumbers[y]
+			_, xUsed := usedNumberExistenceCount[x]
+			_, yUsed := usedNumberExistenceCount[y]
 
-			bothUnused := !xUsed && !yUsed
+			//bothUnused := !xUsed && !yUsed
 
-			if bothExist && bothDifferent && bothUnused {
-				usedNumbers[x] = 1
-				usedNumbers[y] = 1
+			xLessThanUsed := !xUsed
+			yLessThanUsed := !yUsed
+
+			if xUsed {
+				xLessThanUsed = usedNumberExistenceCount[x] < numbers[x]
+			}
+
+			if yUsed {
+				yLessThanUsed = usedNumberExistenceCount[y] < numbers[y]
+			}
+
+			bothXYLessThanUsed := xLessThanUsed && yLessThanUsed
+
+			if bothXYLessThanUsed {
+				isEverythingDistinct = true
+				usedNumberExistenceCount[x] += 1
+				usedNumberExistenceCount[y] += 1
+			} else {
+				isEverythingDistinct = false
+				break
 			}
 		}
+
+		if isEverythingDistinct {
+			countOfTargetValuesFormedByDistinctXAndY++
+		}
+		usedNumberExistenceCount = make(map[int]int)
 	}
 
 	fmt.Println(
 		fmt.Sprintf(
 			"countOfTargetValuesFormedByDistinctXAndY = %d", countOfTargetValuesFormedByDistinctXAndY))
-
-	if numbers != nil {
-		fmt.Println("Populated")
-	}
 }
 
 func NumberMapFromFilepath(
-	filepath string) map[int]uint8 {
-	numbers := make(map[int]uint8)
+	filepath string) map[int]int {
+	numbers := make(map[int]int)
 
 	contentBytes, _ := ioutil.ReadFile(filepath)
 	for _, intStr := range strings.Split(string(contentBytes), "\n") {
 		num, _ := strconv.Atoi(intStr)
-		numbers[num] = 1
+		numbers[num] += 1
 	}
+	//
 
 	return numbers
 }
